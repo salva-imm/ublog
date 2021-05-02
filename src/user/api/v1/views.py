645fa4d005f30faad34e2d6f80d50307ee07b52f
Salvador Imm <mehdi73ee@gmail.com>
2021-05-03 01:51:51 +0430

    Group added with permission classes

--- src/user/api/v1/views.py
+++ src/user/api/v1/views.py
@@ -1,1 +1,6 @@
 from rest_framework.generics import GenericAPIView
+from django.contrib.contenttypes.models import ContentType
+
+
+# ct = ContentType.objects.get(model='group')
+# ct = ContentType.objects.get(model='permission')
