277fc3e99cd67f4d5166abed175d4831c95d1063
Salvador Imm <mehdi73ee@gmail.com>
2021-05-07 03:08:12 +0430

    added comment serializer and view , plus model related_name

---
+++ src/comment/api/v1/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import CommentsSerializer
+
+from comment.models.comment import Comment
+# from blog.models.content_list import ContentList
+
+
+class CommentsView(generics.ListAPIView):
+    queryset = Comment.objects.filter(is_validated=False)
+    serializer_class = CommentsSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = CommentsSerializer(queryset, many=True)
+        return Response(serializer.data)
